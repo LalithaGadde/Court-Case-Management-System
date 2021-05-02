758e56a3ada7b35ee9aa00ac52a1725188c01b64
Lalitha Gadde <64721467+LalithaGadde@users.noreply.github.com>
2021-05-02 16:48:11 +0530

    Add files via upload

---
+++ Xyz.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xyz;
+
+/**
+ *
+ * @author Lalitha Gadde
+ */
+public class Xyz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        firstpage fp = new firstpage();
+    }
+    
+}
